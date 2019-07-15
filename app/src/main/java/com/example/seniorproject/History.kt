3a5b95e3745e8e41b1c8653cc74880d23a9d2b4c
tanthongtan <tan.thongtan1@gmail.com>
2019-07-15 15:47:02 +0700

    HISTORY DONE

--- app/src/main/java/com/example/seniorproject/History.kt
+++ app/src/main/java/com/example/seniorproject/History.kt
@@ -2,6 +2,7 @@ package com.example.seniorproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -15,6 +16,7 @@ class History : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+        Log.d("hello","here")
 
         var usrList: MutableList<String> = mutableListOf()
         var usrReference = FirebaseDatabase.getInstance().getReference("History")
@@ -28,35 +30,44 @@ class History : AppCompatActivity() {
                         var usr = i.getKey()
                         usrList.add(usr!!)
                     }
+
+
+
+                    var outputList: MutableList<String> = mutableListOf()
+                    for (user in usrList){
+                        Log.d("user",user)
+                        if (MyAppApplication.globalUser.equals(user)){
+                            var usrReference = FirebaseDatabase.getInstance().getReference("History/"+user)
+                            usrReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                                override fun onCancelled(p0: DatabaseError) {
+                                }
+
+                                override fun onDataChange(p0: DataSnapshot) {
+                                    if (p0!!.exists()) {
+                                        for (i in p0.children) {
+                                            var date = i.getKey()
+                                            outputList.add(date!!)
+                                            for (j in i.children){
+                                                var record = j.getValue(String::class.java)
+                                                outputList.add(record!!)
+                                            }
+                                        }
+
+                                        val adapter =
+                                            ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, outputList)
+                                        history.adapter = adapter
+
+                                        for (output in outputList){
+                                            Log.d("wtf",output)
+                                        }
+                                    }
+                                }
+                            })
+                        }
+                    }
                 }
             }
         })
-        var outputList: MutableList<String> = mutableListOf()
-        for (user in usrList){
-            if (MyAppApplication.globalUser.equals(user)){
-                var usrReference = FirebaseDatabase.getInstance().getReference("History/"+user)
-                usrReference.addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onCancelled(p0: DatabaseError) {
-                    }
 
-                    override fun onDataChange(p0: DataSnapshot) {
-                        if (p0!!.exists()) {
-                            for (i in p0.children) {
-                                var date = i.getKey()
-                                outputList.add(date!!)
-                                for (j in i.children){
-                                    var record = j.getValue(String::class.java)
-                                    outputList.add(record!!)
-                                }
-                            }
-
-                            val adapter =
-                                ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, outputList)
-                            history.adapter = adapter
-                        }
-                    }
-                })
-            }
-        }
     }
 }
